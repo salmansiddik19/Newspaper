c1c3e9448b40b25195b8494d60625e57b2aed3cd
SSK19 <salmansiddik19@gmail.com>
2020-10-06 21:02:21 +0600

    New updates for Heroku deployment

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'dw3jeeg5s&#2r71dm9y)-y)_yu*^od17s_qwg)gvl#@yz!48je'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
